949b71e9cf7b93ae475ddbf4f2d1bb9d098a4c5a
dganicolas <dganicolas@rafaelalberti.es>
2025-01-22 08:12:42 +0100

    Initial commit

---
+++ FuerzabrutaI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+// See https://aka.ms/new-console-template for more information
+class Program{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the FuerzabrutaI");
+        String filePath = "contra.txt";
+        string[] lines = File.ReadAllLines(filePath);
+        Random rnd = new Random();
+        int numeroRandom = rnd.Next(0,lines.Length);
+        String escogido = lines[numeroRandom];
+        String hash = hashearString(escogido);
+        Console.WriteLine($"contraseña elegida {escogido} y su hash es {hash}");
+        
+        Thread hilo = new Thread();
+    }
+
+    public String hashearString(String escogido)
+    {
+        byte[] tmp = ASCIIEncoding.ASCII.GetBytes(escogido);
+        return ArrayAString(new MD5CryptoServiceProvider().ComputeHash(tmp));
+    }
+    
+    public string 
+    public static void leerArchivo()
+    {
+        
+    }
+    
+    public static 
+}
